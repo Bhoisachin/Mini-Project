763ed12b86ec0deff88bc129ee8a2e32b7aad7b5
Bhoisachin <157878611+Bhoisachin@users.noreply.github.com>
2025-03-11 20:27:07 +0530

    Update Market.py

--- Market.py
+++ Market.py
@@ -7,34 +7,53 @@ import yfinance as yf
 import pandas as pd
 import time as t
 import google.generativeai as genai
-genai.configure(api_key="Enter your API KEY")
-st.header('MARKET.AI')
+from dotenv import load_dotenv
+load_dotenv()
+
+GOOGLE_API_KEY = os.getenv("GOOGLE_API_KEY")
 def AI(que='Hello MARKET.AI'):
+    genai.configure(api_key=GOOGLE_API_KEY)
     st.success('listning....')
     model = genai.GenerativeModel("gemini-1.5-flash")
     response = model.generate_content(que)
+    # print(f'{que}:>>> {response.text}') 
     return response.text
+    
+st.header('MARKET.AI')
+st.sidebar.header('MARKET.AI')
+st.sidebar.title("Avalible stock")
 
-def question_recognizer(qur=st.text_input('enter the prompe')):
+
+def question_recognizer(que=''):
+   
+    qur=st.text_input('Enter the prompt :')
     
     process_question(qur)
+    
 
 with st.spinner('in progress...'):
  t.sleep(5)
 
 def process_question( DF_que=''):
     convert = re.sub('[^a-zA-Z]', ' ', DF_que).lower().split() 
-    os.makedirs('csvfile', exist_ok=True)   
-    for stock_name, stock_symbol in stock_name_list:
-        if re.sub('[^a-zA-Z]', ' ', stock_name).lower() in convert:
+    os.makedirs('csvfile', exist_ok=True) 
+    i=1 
+    for stock_name,stock_symbol in stock_name_list: 
+        
+        st. sidebar.write(f'{i}--{stock_name}')
+        i+=1
+    for stock_name in stock_name_dict:
+        if  re.sub('[^a-zA-Z]', ' ', stock_name).lower() in convert:
+            stock_symbol = stock_name_dict[stock_name]
             if 'close' in convert:
-                return handle_close_operation(stock_name=stock_name,stock_symbol=stock_symbol)
+                return handle_close_operation(stock_name, stock_symbol)
             elif 'high' in convert:
-                return handle_high_operation( stock_name=stock_name, stock_symbol=stock_symbol)
+                return handle_high_operation(stock_name, stock_symbol)
             elif 'low' in convert:
-                return handle_low_operation(stock_name=stock_name,stock_symbol= stock_symbol)
+                return handle_low_operation(stock_name, stock_symbol)
             else:
-                return st.warning(' opration is not found')
+                return st.success(AI(DF_que))
+        
     else:
         if not DF_que:
           return st.success(AI())
@@ -51,16 +70,17 @@ def  handle_close_operation(stock_name,stock_symbol):
             pass
         else:
             os.remove(file_path)
-            st.warning('please update the data')
+            st.warning('please update the data, try again')
             return handle_close_operation(stock_name=stock_name,stock_symbol=stock_symbol)
     else:
-        high=st.number_input('enter the high value')
-        low=st.number_input('enter the low value')
-        open=st.number_input('enter the open value')
-        volume=st.number_input('enter the volume of the stock')
+        st.warning('I dont have any data please insert..')
+        high=st.number_input('Enter the high value:')
+        low=st.number_input('Enter the low value:')
+        open=st.number_input('Enter the open value:')
+        volume=st.number_input('Enter the volume of the stock:')
         if st.button('Enter','enter'):
              value_crater(stock_name,operation='close',df=data_close(high=high,low=low,open=open,volume=volume))  
-             question_recognizer(qur=f'{stock_name} {stock_symbol}')
+             process_question(f'{stock_name} close')
              return 0
         
 def handle_high_operation(stock_name,stock_symbol):
@@ -68,21 +88,22 @@ def handle_high_operation(stock_name,stock_symbol):
     if os.path.exists(file_path):
         df = pd.read_csv(file_path)
         if df['Date'].iloc[-1] == str(date.today()):
-            print('present sir...')
+            
             hight(df=train_data(stock_symbol),hight='high',stock_name=stock_name)
             pass
         else:
             os.remove(file_path)
-            st.warning('please update the data')
+            st.warning('please update the data, try again')
             return handle_high_operation(stock_name=stock_name,stock_symbol=stock_symbol)
     else:
-         close=st.number_input('enter the close value')
-         low=st.number_input('enter the low value')
-         open=st.number_input('enter the open')
-         vol=st.number_input('enter the volume ')
+         st.warning('I dont have any data please insert..')
+         close=st.number_input('Enter the close value:')
+         low=st.number_input('Enter the low value:')
+         open=st.number_input('Enter the open:')
+         vol=st.number_input('Enter the volume: ')
          if st.button('Enter','enter'):
              value_crater(stock_name,operation='high',df=data_high(close=close,low=low,open=open,volume=vol))  
-             question_recognizer(qur=f'{stock_name} {stock_symbol}')
+             process_question(f'{stock_name} high')
              return 0        
   
 def handle_low_operation(stock_name,stock_symbol):
@@ -90,34 +111,114 @@ def handle_low_operation(stock_name,stock_symbol):
     if os.path.exists(file_path):
         df = pd.read_csv(file_path)
         if df['Date'].iloc[-1] == str(date.today()):
-            print('present sir...')
+            
             low(df=train_data(stock_symbol),low='low',stock_name=stock_name)    
         else:
             os.remove(file_path)
-            st.warning('please update the data')
+            st.warning('please update the data, try again')
             return handle_low_operation(stock_name=stock_name,stock_symbol=stock_symbol)
     else:
-         close=st.number_input('enter the close value')
-         high=st.number_input('enter the high value')
-         open=st.number_input('enter the open')
-         vol=st.number_input('enter the volume ')
+         st.warning('I dont have any data please insert..')
+         close=st.number_input('Enter the close value:')
+         high=st.number_input('Enter the high value:')
+         open=st.number_input('Enter the open:')
+         vol=st.number_input('Enter the volume: ')
          if st.button('Enter','enter'):
             #  close, high, open, volume
              value_crater(stock_name,operation='low',df=data_low(close=close,high=high,open=open,volume=vol))  
-             question_recognizer(qur=f'{stock_name} {stock_symbol}')
+             process_question(f'{stock_name} low')
              return 0
 
-stock_name_list = [
-    # Add your stock names and symbols here
-    # Example: ('Tata', 'TATASTEEL'), 
-    ('reliance', 'RELIANCE'),
-    ('tata', 'TATASTEEL.NS'),
-    ('tataconsultancy', 'TCS.NS'),
-    ('state bank of india', 'SBI'),
+stock_name_list  =[
+    ('reliance', 'RELIANCE.NS'),
+    ('TATASTEEL', 'TATASTEEL.NS'),
+    ('TCS', 'TCS.NS'),
+    ('SBI', 'SBI'),
     ('ITC', 'ITC.NS'),
+    ('RivianAutomotive', 'RIVN'),
+    ('HersHealth', 'HIMS'),
+    ('NVIDIACorporation', 'NVDA'),
+    ('HDFCBank ', 'HDFCBANK.NS'),
+    ('CassavaSciences', 'SAVA'),
+    ('TATAMOTORS', 'TATAMOTORS.NS'),
+    ('TATAINVEST', 'TATAINVEST.BO'),
+    ('TataAIA ', '0P0000NQJX.BO'),
+    ('Lockheed', 'LMT'),
+    ('AmericanBeacon', 'ADNIX'),
+    ('GameStop', 'GME'),
+    ('ICICIBank', 'IBN'),
+    ('BandhanBanking', '0P000125VF.BO'),
+    ('TheFederalBank', 'FEDERALBNK.NS'),
+    ('KotakMahindraBank', 'KOTAKBANK.BO'),
+    ('OLA', 'OLAELEC.NS'),
+    ('JPP', 'JPPOWER.BO'),
+    ('NCL ', 'NCLRESE.BO'),
+    ('MTNL', 'MTNL.NS'),
+    ('TataTeleservices', 'TTML.BO'),
+    ('AngelOne', 'ANGELONE.NS'),
+    ('Zomato', 'ZOMATO.NS'),
+    ('Microsoft', 'MSFT'),
+    ('google', 'GOOG'),
+    ('SWIGGY', 'SWIGGY.NS'),
+    ('Wipro', 'WIPRO.NS'),
+    ('AdaniPower', 'ADANIPOWER.NS'),
+    ('AdaniEnterprises', 'ADANIENT.NS'),
+    ('LifeInsurance', 'LICI.NS'),
+    ('HindustanUni', 'HINDUNILVR.NS'),
+    ('LarsenToubro', 'LT.NS'),
+    ('HCL', 'HCLTECH.NS'),
+    ('Mahindra', 'M&M.NS'),
+    ('NTPC', 'NTPC.NS'),
+    ('MarutiSuzuki', 'MARUTI.BO'),
+    ('UltraTechCement', 'UCLQF'),
+    ('OilNaturalGas', 'ONGC.BO'),
+    ('Siemens', 'SIEMENS.BO'),
+    ('PricingCulture', 'CLTBWOWOM'),
+    ('CoalIndia', 'COALINDIA.NS'),
+    ('BajajAuto', 'BAJAJ-AUTO.BO'),
+    ('Trent', 'TRENT.NS'),
+    ('BharatElec', 'BEL.NS'),
+    ('Hindustan', 'HINDZINC.BO'),
+    ('Hindustan', 'HINDZINC.BO'),
+    ('DLF', 'DLF.BO'),
+    ('IndianRailway', 'IRFC.BO'),
+    ('IndianOil', 'IOC.BO'),
+    ('AdaniGreenEnergy', 'ADANIGREEN.NS'),
+    ('GrasimIndustries', 'GRASIM.NS'),
+    ('TechMahindra', 'TECHM.BO'),
+    ('PowerFinanceCorporation', 'PFC.BO'),
+    ('InterGlobeAviation', 'INDIGO.NS'),
+    ('DivisLaboratories', 'DIVISLAB.BO'),
+    ('Abbott', 'ABBOTINDIA.BO'),
+    ('PidiliteIndustries', 'PIDILITIND.BO'),
+    ('HindalcoIndustries', 'HINDALCO.NS'),
+    ('EcoRecycling', 'ECORECO.BO'),
+    ('HyundaiMotor', 'HYUNDAI.NS'),
+    ('AmbujaCements', 'AMBUJACEM.BO'),
+    ('GAIL(India)', 'GQI.F'),
+    ('BankofBaroda.', 'BANKBARODA.NS'),
+    ('MacrotecDevelopers', 'LODHA.NS'),
+    ('EicherMotors', 'EICHERMOT.BO'),
+    ('PunjabBank.', 'PNB.NS'),
+    ('JSW', 'JSWHL.NS'),
+    ('TataPower', 'TATAPOWER.NS'),
+    ('IHCL', 'INDHOTEL.BO'),
+    ('NTPC', 'NTPCGREEN.BO'),
+    ('TVS ', 'TVSMOTOR.NS'),
+    ('SHRIRAM ', 'SHRIRAMFIN.NS'),
+    ('CGPower ', 'CGPOWER.NS'),
+    ('Cipla ', 'CIPLA.NS'),
+    ('Britannia ', 'BRITANNIA.BO'),
+    ('Samvardhana  ', 'MOTHERSON.BO'),
+    ('MaxHealth', 'MAXHEALTH.NS'),
+    ('Godrej', 'GODREJCP.BO'),
+    ('TorrentPharm', 'TORNTPHARM.BO'),
+    ('POLYCAB', 'POLYCAB.NS'), 
+    ('axisgold', 'AXISGOLD.NS'), 
 ]
+stock_name_dict=dict(stock_name_list)
 def train_data(stock_name='GOOG'):
-    start = '2012-01-01'
+    start = '2000-01-01'
     end = date.today()
     stock = stock_name
     data = yf.download(stock, start, end)
@@ -131,6 +232,8 @@ def train_data(stock_name='GOOG'):
     return data
 
 def close(df,close,stock_name):
+     
+    start_time = t.time() 
     df1=pd.read_csv(f'csvfile/{stock_name}NS_{close}.csv')
     # Drop the 'Date' column from df1
     df1.drop(columns=['Date'],axis=1, inplace=True)  
@@ -141,17 +244,20 @@ def close(df,close,stock_name):
     s=s.to_numpy()
     actual_price=s.tolist()
 
-    x=df.drop( columns=['Close','Adj Close'],axis=1)
+    x=df.drop( columns=['Close'],axis=1)
+    print('hallow')
     y=df['Close']
     lr=LinearRegression()
     lr.fit(x,y)
     # Ensure df1 is a DataFrame with numeric values
     y_pred=lr.predict(df1)
     predected_price=  y_pred.tolist()
+    end_time = t.time()
+    processing_time = end_time - start_time
+    st.success(f"Processing Time: {processing_time:.4f} seconds")
 
-    
-    
-    return st.success(f"{AI(f'hallow ai it my actual price:{actual_price} and it my predected price :{predected_price} can you sumraized the stock can i bye or sale stock nema is {stock_name}')}  predected price:{predected_price}")
+
+    return st.success(f"{AI(f'Hello AI its my actual price:{actual_price[0]} and its my predected price :{predected_price[0]} can you summerized the stock and can I bye or sale stock name is {stock_name}')}")
 
 def hight(df, hight,stock_name):
    df1=pd.read_csv(f'csvfile/{stock_name}NS_{hight}.csv')
@@ -163,13 +269,13 @@ def hight(df, hight,stock_name):
    s=df['High'].tail(1)
    s=s.to_numpy()
    actual_price=s.tolist()
-   x=df.drop( columns=['High','Adj Close'],axis=1)
+   x=df.drop( columns=['High',],axis=1)
    y=df['High']
    lr=LinearRegression()
    lr.fit(x,y)
    y_pred=lr.predict(df1)
    predected_price=  y_pred.tolist()
-   return st.success(f"{AI(f'hallow ai it my actual price:{actual_price} and it my predected price :{predected_price} can you sumraized the stock can i bye or sale stock nema is {stock_name}')}  predected price:{predected_price}")
+   return st.success(f"{AI(f'Hello AI its my actual price:{actual_price[0]} and its my predected price :{predected_price[0]} can you summerized the stock and can I bye or sale stock name is {stock_name}')}")
 
 def low(df,low,stock_name):
    df1=pd.read_csv(f'csvfile/{stock_name}NS_{low}.csv')
@@ -180,13 +286,13 @@ def low(df,low,stock_name):
    s=df['Low'].tail(1)
    s=s.to_numpy()
    actual_price=s.tolist()
-   x=df.drop( columns=['Low','Adj Close'],axis=1)
+   x=df.drop( columns=['Low',],axis=1)
    y=df['Low']
    lr=LinearRegression()
    lr.fit(x,y)
    y_pred=lr.predict(df1)
    predected_price=  y_pred.tolist()
-   return st.success(f"{AI(f'hallow ai it my actual price:{actual_price} and it my predected price :{predected_price} can you sumraized the stock can i bye or sale stock nema is {stock_name}')}  predected price:{predected_price}")
+   return st.success(f"{AI(f'Hello AI its my actual price:{actual_price[0]} and its my predected price :{predected_price[0]} can you summerized the stock and can I bye or sale stock name is {stock_name}')}")
 
 
 def data_close(high, low, open, volume):
@@ -225,3 +331,4 @@ def value_crater(stock_symbol, operation, df):
     df.to_csv(file_path, index=False)
 
 question_recognizer()
+
